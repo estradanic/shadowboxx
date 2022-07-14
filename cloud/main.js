44c038fd481397d2eb6503c2be4ec5a18030a5f9
Nicholas Estrada <116nic97@gmail.com>
2022-07-13 17:17:25 -0700

    Making image resizing actually work

--- cloud/main.js
+++ cloud/main.js
@@ -1,24 +1,16 @@
-const Image = require("parse-image");
+const resizeImg = require("resize-image-buffer");
 
-Parse.Cloud.beforeSave("Image", async (request, response) => {
+Parse.Cloud.beforeSave("Image", async (request) => {
   const imageRecord = request.object;
-  if (!imageRecord.get("file") || !imageRecord.dirty("file")) {
-    response.success();
-    return imageRecord;
+  if (!!imageRecord.get("file") && (imageRecord.dirty("file") || !imageRecord.get("fileThumb") || !imageRecord.get("fileMobile"))) {
+    try {
+      const { buffer } = await Parse.Cloud.httpRequest({ url: imageRecord.get("file").url() });
+      const thumbnail = (await resizeImg(buffer, { width: 64 })).toString("base64");
+      const mobile = (await resizeImg(buffer, { width: 900 })).toString("base64");
+      imageRecord.set("fileThumb", await new Parse.File(imageRecord.get("name") + "_thumb.webp", { base64: thumbnail }).save());
+      imageRecord.set("fileMobile", await new Parse.File(imageRecord.get("name") + "_mobile.webp", { base64: mobile }).save());
+    } catch (e) {
+      throw e;
+    }
   }
-
-  try {
-    const buffer = await imageRecord.get("file").getData();
-    const original = (await (await new Image().setData(buffer)).setFormat("webp")).data().toString("base64");
-    const thumbnail = (await (await (await new Image().setData(buffer)).scale({width: 64})).setFormat("webp")).data().toString("base64");
-    const mobile = (await (await (await new Image().setData(buffer)).scale({width: 900})).setFormat("webp")).data().toString("base64");
-    imageRecord.set("file", await new Parse.File(imageRecord.get("name") + ".webp", {base64: original}).save());
-    imageRecord.set("fileThumb", await new Parse.File(imageRecord.get("name") + "_thumb.webp", {base64: thumbnail}).save());
-    imageRecord.set("fileMobile", await new Parse.File(imageRecord.get("name") + "_mobile.webp", {base64: mobile}).save());
-  } catch (e) {
-    throw e;
-  }
-
-  response.success();
-  return imageRecord;
 });
