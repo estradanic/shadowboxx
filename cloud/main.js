e9a3935512b6208d98e7c96fba275b21176d58ff
Nicholas Estrada <116nic97@gmail.com>
2022-07-13 21:21:19 -0700

    Smaller mobile images (700px width)

--- cloud/main.js
+++ cloud/main.js
@@ -6,7 +6,7 @@ Parse.Cloud.beforeSave("Image", async (request) => {
     try {
       const { buffer } = await Parse.Cloud.httpRequest({ url: imageRecord.get("file").url() });
       const thumbnail = (await resizeImg(buffer, { width: 64, format: "png" })).toString("base64");
-      const mobile = (await resizeImg(buffer, { width: 900, format: "png" })).toString("base64");
+      const mobile = (await resizeImg(buffer, { width: 700, format: "png" })).toString("base64");
       imageRecord.set("fileThumb", await new Parse.File(imageRecord.get("name") + "_thumb.png", { base64: thumbnail }).save());
       imageRecord.set("fileMobile", await new Parse.File(imageRecord.get("name") + "_mobile.png", { base64: mobile }).save());
     } catch (e) {
