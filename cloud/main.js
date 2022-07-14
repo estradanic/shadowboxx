81a65d7f6c2793ad8f783d48f897122f846af959
Nicholas Estrada <116nic97@gmail.com>
2022-07-13 21:17:47 -0700

    Not lying about the image format any more

--- cloud/main.js
+++ cloud/main.js
@@ -5,10 +5,10 @@ Parse.Cloud.beforeSave("Image", async (request) => {
   if (!!imageRecord.get("file") && (imageRecord.dirty("file") || !imageRecord.get("fileThumb") || !imageRecord.get("fileMobile"))) {
     try {
       const { buffer } = await Parse.Cloud.httpRequest({ url: imageRecord.get("file").url() });
-      const thumbnail = (await resizeImg(buffer, { width: 64 })).toString("base64");
-      const mobile = (await resizeImg(buffer, { width: 900 })).toString("base64");
-      imageRecord.set("fileThumb", await new Parse.File(imageRecord.get("name") + "_thumb.webp", { base64: thumbnail }).save());
-      imageRecord.set("fileMobile", await new Parse.File(imageRecord.get("name") + "_mobile.webp", { base64: mobile }).save());
+      const thumbnail = (await resizeImg(buffer, { width: 64, format: "png" })).toString("base64");
+      const mobile = (await resizeImg(buffer, { width: 900, format: "png" })).toString("base64");
+      imageRecord.set("fileThumb", await new Parse.File(imageRecord.get("name") + "_thumb.png", { base64: thumbnail }).save());
+      imageRecord.set("fileMobile", await new Parse.File(imageRecord.get("name") + "_mobile.png", { base64: mobile }).save());
     } catch (e) {
       throw e;
     }
