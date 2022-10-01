076b70021eeaeb9135ee00735ccaa84a65bf1fb8
Nicholas Estrada <nicholasjestrada@protonmail.com>
2022-10-01 15:47:55 -0700

    Attempting to configure b4a accountkey but properly

--- .github/actions/deploy/entrypoint.sh
+++ .github/actions/deploy/entrypoint.sh
@@ -1,7 +1,32 @@
 #!/bin/sh -l
 
-b4a configure accountkey $2
+APP_ID = $1
+ACCOUNT_KEY = $2
+EMAIL = $3
+APP_NAME = $4
 
-echo "Hello! Your AppID is: $1"
-echo "Your account key is: $2"
-echo "Your email is: $3"
+if [ -z $APP_ID ]
+then
+  echo "appid is empty or not provided"
+  exit 1
+fi
+
+if [ -z $ACCOUNT_KEY ]
+then
+  echo "accountkey is empty or not provided"
+  exit 1
+fi
+
+if [ -z $EMAIL ]
+then
+  echo "email is empty or not provided"
+  exit 1
+fi
+
+if [ -z $APP_NAME ]
+then
+  echo "appname is empty or not provided"
+  exit 1
+fi
+
+printf $ACCOUNT_KEY | b4a configure accountkey
