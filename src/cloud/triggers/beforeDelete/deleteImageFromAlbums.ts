80d0020181b6cb6e92921d10420e4e19a166fb70
Nicholas Estrada <github@nicholasjestrada.aleeas.com>
2023-01-18 15:17:24 -0800

    Adding some logging and a small error handle

--- src/cloud/triggers/beforeDelete/deleteImageFromAlbums.ts
+++ src/cloud/triggers/beforeDelete/deleteImageFromAlbums.ts
@@ -1,24 +1,19 @@
 /** Function to delete image from the albums it appears in before destruction */
 const deleteImageFromAlbums = async (image: Parse.Object) => {
-  console.log("Deleting image from albums");
+  console.log(`Deleting image ${image.id} from albums`);
   const albums = await new Parse.Query("Album")
     .contains("images", image.id)
     .find({ useMasterKey: true });
   console.log("Found albums", albums);
   await Promise.all(
     albums.map(async (album) => {
-      console.log("Deleting image from album", album.id);
+      console.log(`Deleting image ${image.id} from album`, album.id);
       const images = album.get("images") as string[];
       album.set(
         "images",
         images.filter((id) => id !== image.id)
       );
-      console.log(
-        "Saving album",
-        album.id,
-        "with images",
-        album.get("images") ?? "[]"
-      );
+      console.log("Saving album", album.id);
       await album.save(null, { useMasterKey: true });
       console.log("Saved album", album.id);
     })
