22348491690f0a8a910e455bcca06f22aa123dcb
Nicholas Estrada <github@nicholasjestrada.aleeas.com>
2023-01-17 16:02:17 -0800

    Improving communication between client and service worker

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,7 +2,3 @@ export const DEFAULT_PAGE_SIZE = 25;
 
 export const PARSE_DB_NAME = "parseDB";
 export const PARSE_STORE_NAME = "parseStore";
-
-export const SHARE_TARGET_DB_NAME = "shareTargetDB";
-export const SHARE_TARGET_STORE_NAME = "shareTargetStore";
-export const SHARE_TARGET_STORE_KEY = "sharedFiles";
---
+++ src/serviceWorker/sharedExports.ts
@@ -0,0 +1,3 @@
+export const SHARE_TARGET_DB_NAME = "shareTargetDB";
+export const SHARE_TARGET_STORE_NAME = "shareTargetStore";
+export const SHARE_TARGET_STORE_KEY = "sharedFiles";
