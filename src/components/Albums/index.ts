ce746412fabd0e7cf8ebde82261399e81c1116f9
Nicholas Estrada <16547547+estradanic@users.noreply.github.com>
2023-01-20 18:46:12 +0000

    Making ux better for sharing from device

--- src/components/Albums/index.ts
+++ src/components/Albums/index.ts
@@ -1,2 +1,4 @@
 export { default as AlbumCard } from "./AlbumCard";
 export { default as AlbumFormDialog } from "./AlbumFormDialog";
+export { default as SmallAlbumCard } from "./SmallAlbumCard";
+export { default as NoAlbums } from "./NoAlbums";
--- src/components/NetworkDetector/index.ts
+++ src/components/NetworkDetector/index.ts
@@ -1,2 +1,3 @@
 export { default as Online } from "./Online";
 export { default as Offline } from "./Offline";
+export { default as NoConnection } from "./NoConnection";
