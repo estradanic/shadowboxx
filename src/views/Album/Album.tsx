ee8a00e1cabb9cb28b8eb0ccac37be976d43f4a2
Nicholas Estrada <github@nicholasjestrada.aleeas.com>
2023-10-22 16:59:57 -0700

    [#347] Broke out Album.tsx into LoadingContent and SuccessContent

--- src/views/Album/Album.tsx
+++ src/views/Album/Album.tsx
@@ -1,70 +1,29 @@
-import React, { memo, useCallback, useState } from "react";
+import React, { memo } from "react";
 import {
-  AlbumFormDialog,
   BackButton,
-  FancyTitleTypographySkeleton,
-  ImagesSkeleton,
   PageContainer,
-  Fab,
-  useSnackbar,
-  Online,
-  Timeline,
-  FancyTypography,
 } from "../../components";
-import { useLocation, useParams, useSearchParams } from "react-router-dom";
+import { useLocation, useParams } from "react-router-dom";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
-import { makeStyles, Theme } from "@material-ui/core/styles";
-import EditIcon from "@material-ui/icons/Edit";
-import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
+import { makeStyles } from "@material-ui/core/styles";
+import { useQuery } from "@tanstack/react-query";
 import { Strings } from "../../resources";
-import { ParseImage, ParseAlbum } from "../../classes";
-import { FancyTitleTypography, Void, Images } from "../../components";
-import { DEFAULT_PAGE_SIZE } from "../../constants";
+import { ParseAlbum } from "../../classes";
+import { Void } from "../../components";
 import { useView } from "../View";
-import OwnerImageDecoration from "../../components/Image/Decoration/OwnerImageDecoration";
-import useFlatInfiniteQueryData from "../../hooks/Query/useFlatInfiniteQueryData";
-import { FormControlLabel, Switch } from "@material-ui/core";
-import { VariableColor } from "../../types";
-import ShareImageDecoration from "../../components/Image/Decoration/ShareImageDecoration";
 import useRandomColor from "../../hooks/useRandomColor";
 import useQueryConfigs from "../../hooks/Query/useQueryConfigs";
-import useInfiniteQueryConfigs from "../../hooks/Query/useInfiniteQueryConfigs";
-import useInfiniteScroll from "../../hooks/useInfiniteScroll";
 import { useNetworkDetectionContext } from "../../contexts/NetworkDetectionContext";
-import useFilterBar from "../../components/FilterBar/useFilterBar";
-import FilterBar from "../../components/FilterBar/FilterBar";
+import LoadingContent from "./LoadingContent";
+import SuccessContent from "./SuccessContent";
 
-type UseStylesParams = {
-  randomColor: VariableColor;
-};
-
-const useStyles = makeStyles((theme: Theme) => ({
+const useStyles = makeStyles(() => ({
   svgContainer: {
     textAlign: "center",
   },
   svgText: {
     fontSize: "medium",
-  },
-  controls: {
-    marginTop: theme.spacing(4),
-  },
-  switchText: {
-    fontSize: "x-large",
-  },
-  switchBase: {
-    "&&": {
-      color: ({ randomColor }: UseStylesParams) =>
-        theme.palette[randomColor ?? "primary"].light,
-    },
-  },
-  switchChecked: {},
-  switchTrack: {
-    "&&&": {
-      backgroundColor: ({ randomColor }: UseStylesParams) =>
-        theme.palette[randomColor ?? "primary"].dark,
-      opacity: 1,
-    },
   },
 }));
 
@@ -74,8 +33,6 @@ const useStyles = makeStyles((theme: Theme) => ({
 const Album = memo(() => {
   useView("Album");
   const { id } = useParams<{ id: string }>();
-  const [search, setSearch] = useSearchParams();
-  const timelineView = search.get("timeline") === "true";
   const location = useLocation() as { state: { previousLocation?: Location } };
   const randomColor = useRandomColor();
   const classes = useStyles({ randomColor });
@@ -84,190 +41,20 @@ const Album = memo(() => {
     getAlbumFunction,
     getAlbumQueryKey,
     getAlbumOptions,
-    getTagsByImageIdFunction,
-    getTagsByImageIdOptions,
-    getTagsByImageIdQueryKey,
   } = useQueryConfigs();
-  const {
-    getImagesByIdInfiniteFunction,
-    getImagesByIdInfiniteQueryKey,
-    getImagesByIdInfiniteOptions,
-  } = useInfiniteQueryConfigs();
+
   const { data: album, status: albumStatus } = useQuery<ParseAlbum, Error>(
     getAlbumQueryKey(id),
     () => getAlbumFunction(online, id, { showErrorsInSnackbar: true }),
     getAlbumOptions()
   );
-  const {
-    sortDirection,
-    debouncedCaptionSearch,
-    tagSearch,
-    ...restFilterBarProps
-  } = useFilterBar();
-  const { data: tags } = useQuery<string[], Error>(
-    getTagsByImageIdQueryKey(album?.images ?? []),
-    () => getTagsByImageIdFunction(album?.images ?? []),
-    getTagsByImageIdOptions({ enabled: !!album?.images })
-  );
-  const {
-    data,
-    status: imagesStatus,
-    fetchNextPage,
-    isFetchingNextPage,
-    refetch,
-    isRefetching,
-  } = useInfiniteQuery<ParseImage[], Error>(
-    getImagesByIdInfiniteQueryKey(album?.images ?? [], {
-      sortDirection,
-      captionSearch: debouncedCaptionSearch,
-      captions: album?.captions ?? {},
-      tagSearch,
-    }),
-    ({ pageParam: page = 0 }) =>
-      getImagesByIdInfiniteFunction(
-        online,
-        album?.images ?? [],
-        {
-          sortDirection,
-          captionSearch: debouncedCaptionSearch,
-          captions: album?.captions ?? {},
-          tagSearch,
-        },
-        {
-          showErrorsInSnackbar: true,
-          page,
-          pageSize: DEFAULT_PAGE_SIZE,
-        }
-      ),
-    getImagesByIdInfiniteOptions({ enabled: !!album?.images })
-  );
-  useInfiniteScroll(fetchNextPage, { canExecute: !isFetchingNextPage });
-  const [editMode, setEditMode] = useState<boolean>(false);
-  const { enqueueSuccessSnackbar, enqueueErrorSnackbar } = useSnackbar();
-
-  const images = useFlatInfiniteQueryData(data);
-
-  const getImageDecorations = useCallback(
-    async (image: ParseImage) => {
-      if (timelineView) {
-        return [];
-      }
-      return [
-        <OwnerImageDecoration
-          UserAvatarProps={{
-            UseUserInfoParams: { userPointer: image.owner },
-          }}
-        />,
-        <ShareImageDecoration image={image} />,
-      ];
-    },
-    [timelineView]
-  );
-
-  const getImageCaption = useCallback(
-    async (image: ParseImage) => {
-      return album?.captions?.[image.objectId] ?? "";
-    },
-    [album?.captions]
-  );
-
-  const getImageProps = useCallback(
-    async (image: ParseImage) => {
-      const decorations = await getImageDecorations(image);
-      const caption = await getImageCaption(image);
-      return {
-        decorations,
-        caption,
-      };
-    },
-    [getImageDecorations, getImageCaption]
-  );
-
-  const filterBar = (
-    <FilterBar
-      tagSearch={tagSearch}
-      sortDirection={sortDirection}
-      tagOptions={tags}
-      {...restFilterBarProps}
-    />
-  );
 
   return (
     <PageContainer>
       {albumStatus === "loading" ? (
-        <>
-          <FancyTitleTypographySkeleton outlineColor={randomColor} />
-          <ImagesSkeleton />
-        </>
+        <LoadingContent randomColor={randomColor} />
       ) : albumStatus === "success" && album ? (
-        <>
-          <AlbumFormDialog
-            resetOnConfirm
-            value={album.attributes}
-            open={editMode}
-            handleCancel={() => setEditMode(false)}
-            handleConfirm={async (attributes, changes) => {
-              setEditMode(false);
-              try {
-                await album.update(attributes, changes);
-                await refetch();
-                enqueueSuccessSnackbar(Strings.success.saved);
-              } catch (error: any) {
-                console.error(error);
-                enqueueErrorSnackbar(Strings.error.editingAlbum);
-              }
-            }}
-          />
-          <Grid item sm={8}>
-            <FancyTitleTypography outlineColor={randomColor}>
-              {album.name}
-            </FancyTitleTypography>
-          </Grid>
-          <Grid item sm={8} className={classes.controls}>
-            <FormControlLabel
-              control={
-                <Switch
-                  classes={{
-                    switchBase: classes.switchBase,
-                    checked: classes.switchChecked,
-                    track: classes.switchTrack,
-                  }}
-                  checked={!!timelineView}
-                  onClick={() =>
-                    setSearch({ timeline: timelineView ? "false" : "true" })
-                  }
-                />
-              }
-              label={
-                <FancyTypography className={classes.switchText}>
-                  {Strings.label.timelineView}
-                </FancyTypography>
-              }
-            />
-          </Grid>
-          {timelineView ? (
-            <Timeline
-              filterBar={filterBar}
-              getImageProps={getImageProps}
-              status={isRefetching ? "refetching" : imagesStatus}
-              images={images}
-              outlineColor={randomColor}
-            />
-          ) : (
-            <Images
-              filterBar={filterBar}
-              getImageProps={getImageProps}
-              status={isRefetching ? "refetching" : imagesStatus}
-              images={images}
-              outlineColor={randomColor}
-            />
-          )}
-          <Online>
-            <Fab onClick={() => setEditMode(true)}>
-              <EditIcon />
-            </Fab>
-          </Online>
-        </>
+        <SuccessContent album={album} randomColor={randomColor} />
       ) : (
         <Grid item className={classes.svgContainer}>
           <Void height="40vh" />
---
+++ src/views/Album/SuccessContent.tsx
@@ -0,0 +1,248 @@
+import React, { useCallback, useState } from "react";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Grid from "@material-ui/core/Grid";
+import { makeStyles, Theme } from "@material-ui/core/styles";
+import Switch from "@material-ui/core/Switch";
+import { useSearchParams } from "react-router-dom";
+import ParseAlbum from "../../cloud/shared/classes/ParseAlbum";
+import AlbumFormDialog from "../../components/Albums/AlbumFormDialog";
+import Fab from "../../components/Button/Fab";
+import Images from "../../components/Image/Images";
+import Timeline from "../../components/Image/Timeline";
+import Online from "../../components/NetworkDetector/Online";
+import { useSnackbar } from "../../components/Snackbar";
+import { FancyTitleTypography, FancyTypography } from "../../components/Typography";
+import {Strings} from "../../resources";
+import VariableColor from "../../types/VariableColor";
+import EditIcon from "@material-ui/icons/Edit";
+import { ParseImage } from "../../classes";
+import OwnerImageDecoration from "../../components/Image/Decoration/OwnerImageDecoration";
+import ShareImageDecoration from "../../components/Image/Decoration/ShareImageDecoration";
+import FilterBar from "../../components/FilterBar/FilterBar";
+import useFilterBar from "../../components/FilterBar/useFilterBar";
+import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
+import useQueryConfigs from "../../hooks/Query/useQueryConfigs";
+import useInfiniteQueryConfigs from "../../hooks/Query/useInfiniteQueryConfigs";
+import { useNetworkDetectionContext } from "../../contexts/NetworkDetectionContext";
+import { DEFAULT_PAGE_SIZE } from "../../constants";
+import useInfiniteScroll from "../../hooks/useInfiniteScroll";
+import useFlatInfiniteQueryData from "../../hooks/Query/useFlatInfiniteQueryData";
+
+type UseStylesParams = {
+  randomColor: VariableColor;
+};
+
+const useStyles = makeStyles((theme: Theme) => ({
+  controls: {
+    marginTop: theme.spacing(4),
+  },
+  switchText: {
+    fontSize: "x-large",
+  },
+  switchBase: {
+    "&&": {
+      color: ({ randomColor }: UseStylesParams) =>
+        theme.palette[randomColor ?? "primary"].light,
+    },
+  },
+  switchChecked: {},
+  switchTrack: {
+    "&&&": {
+      backgroundColor: ({ randomColor }: UseStylesParams) =>
+        theme.palette[randomColor ?? "primary"].dark,
+      opacity: 1,
+    },
+  },
+}));
+
+type SuccessContentProps = {
+  album: ParseAlbum,
+  randomColor: VariableColor,
+};
+
+const SuccessContent = ({album, randomColor}: SuccessContentProps) => {
+  const [editMode, setEditMode] = useState<boolean>(false);
+  const {enqueueSuccessSnackbar, enqueueErrorSnackbar} = useSnackbar();
+  const classes = useStyles({randomColor});
+  const [search, setSearch] = useSearchParams();
+  const timelineView = search.get("timeline") === "true";
+  const { online } = useNetworkDetectionContext();
+
+  const {
+    getTagsByImageIdFunction,
+    getTagsByImageIdOptions,
+    getTagsByImageIdQueryKey,
+  } = useQueryConfigs();
+  const {
+    getImagesByIdInfiniteFunction,
+    getImagesByIdInfiniteQueryKey,
+    getImagesByIdInfiniteOptions,
+  } = useInfiniteQueryConfigs();
+
+  const {
+    sortDirection,
+    debouncedCaptionSearch,
+    tagSearch,
+    ...restFilterBarProps
+  } = useFilterBar();
+  const { data: tags } = useQuery<string[], Error>(
+    getTagsByImageIdQueryKey(album.images),
+    () => getTagsByImageIdFunction(album.images),
+    getTagsByImageIdOptions()
+  );
+
+  const {
+    data,
+    status: imagesStatus,
+    fetchNextPage,
+    isFetchingNextPage,
+    refetch,
+    isRefetching,
+  } = useInfiniteQuery<ParseImage[], Error>(
+    getImagesByIdInfiniteQueryKey(album.images ?? [], {
+      sortDirection,
+      captionSearch: debouncedCaptionSearch,
+      captions: album.captions,
+      tagSearch,
+    }),
+    ({ pageParam: page = 0 }) =>
+      getImagesByIdInfiniteFunction(
+        online,
+        album.images,
+        {
+          sortDirection,
+          captionSearch: debouncedCaptionSearch,
+          captions: album.captions,
+          tagSearch,
+        },
+        {
+          showErrorsInSnackbar: true,
+          page,
+          pageSize: DEFAULT_PAGE_SIZE,
+        }
+      ),
+    getImagesByIdInfiniteOptions({ enabled: !!album.images })
+  );
+  
+  useInfiniteScroll(fetchNextPage, { canExecute: !isFetchingNextPage });
+
+  const getImageDecorations = useCallback(
+    async (image: ParseImage) => {
+      if (timelineView) {
+        return [];
+      }
+      return [
+        <OwnerImageDecoration
+          UserAvatarProps={{
+            UseUserInfoParams: { userPointer: image.owner },
+          }}
+        />,
+        <ShareImageDecoration image={image} />,
+      ];
+    },
+    [timelineView]
+  );
+
+  const getImageCaption = useCallback(
+    async (image: ParseImage) => {
+      return album.captions?.[image.objectId] ?? "";
+    },
+    [album.captions]
+  );
+
+  const getImageProps = useCallback(
+    async (image: ParseImage) => {
+      const decorations = await getImageDecorations(image);
+      const caption = await getImageCaption(image);
+      return {
+        decorations,
+        caption,
+      };
+    },
+    [getImageDecorations, getImageCaption]
+  );
+
+  const images = useFlatInfiniteQueryData(data);
+
+  const filterBar = (
+    <FilterBar
+      tagSearch={tagSearch}
+      sortDirection={sortDirection}
+      tagOptions={tags}
+      {...restFilterBarProps}
+    />
+  );
+
+	return (
+    <>
+      <AlbumFormDialog
+        resetOnConfirm
+        value={album.attributes}
+        open={editMode}
+        handleCancel={() => setEditMode(false)}
+        handleConfirm={async (attributes, changes) => {
+          setEditMode(false);
+          try {
+            await album.update(attributes, changes);
+            await refetch();
+            enqueueSuccessSnackbar(Strings.success.saved);
+          } catch (error: any) {
+            console.error(error);
+            enqueueErrorSnackbar(Strings.error.editingAlbum);
+          }
+        }}
+      />
+      <Grid item sm={8}>
+        <FancyTitleTypography outlineColor={randomColor}>
+          {album.name}
+        </FancyTitleTypography>
+      </Grid>
+      <Grid item sm={8} className={classes.controls}>
+        <FormControlLabel
+          control={
+            <Switch
+              classes={{
+                switchBase: classes.switchBase,
+                checked: classes.switchChecked,
+                track: classes.switchTrack,
+              }}
+              checked={!!timelineView}
+              onClick={() =>
+                setSearch({ timeline: timelineView ? "false" : "true" })
+              }
+            />
+          }
+          label={
+            <FancyTypography className={classes.switchText}>
+              {Strings.label.timelineView}
+            </FancyTypography>
+          }
+        />
+      </Grid>
+      {timelineView ? (
+        <Timeline
+          filterBar={filterBar}
+          getImageProps={getImageProps}
+          status={isRefetching ? "refetching" : imagesStatus}
+          images={images}
+          outlineColor={randomColor}
+        />
+      ) : (
+        <Images
+          filterBar={filterBar}
+          getImageProps={getImageProps}
+          status={isRefetching ? "refetching" : imagesStatus}
+          images={images}
+          outlineColor={randomColor}
+        />
+      )}
+      <Online>
+        <Fab onClick={() => setEditMode(true)}>
+          <EditIcon />
+        </Fab>
+      </Online>
+    </>
+	);
+};
+
+export default SuccessContent;
