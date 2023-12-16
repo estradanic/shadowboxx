c067ecd898978e4873862f8a0e8c205d193c1b22
Nicholas Estrada <github@nicholasjestrada.aleeas.com>
2023-12-16 15:35:38 -0800

    Some code cleanup and tweaks in previews

--- src/components/Image/ImageJobPlaceholder.tsx
+++ src/components/Image/ImageJobPlaceholder.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Grid from "@material-ui/core/Grid";
 import { ParseImage } from "../../classes";
 import { JobInfo } from "../../contexts/JobContext";
@@ -28,18 +28,17 @@ const ImageJobPlaceholder = ({ job, className }: ImageJobPlaceholderProps) => {
   const type = ACCEPTABLE_VIDEO_TYPES.includes(job.file?.type ?? "")
     ? "video"
     : "image";
-  const file = job.file;
-  if (file) {
-    const reader = new FileReader();
-    reader.addEventListener(
-      "load",
-      () => {
-        setDataUrl(reader.result as string);
-      },
-      { once: true }
-    );
-    reader.readAsDataURL(file);
-  }
+
+  useEffect(() => {
+    const file = job.file;
+    if (file) {
+      const blobUrl = URL.createObjectURL(file);
+      setDataUrl(blobUrl);
+      return () => {
+        URL.revokeObjectURL(blobUrl);
+      };
+    }
+  }, [job.file, setDataUrl]);
 
   return (
     <Grid item xs={12} md={6} lg={4} xl={3} className={className}>
@@ -58,7 +57,7 @@ const ImageJobPlaceholder = ({ job, className }: ImageJobPlaceholderProps) => {
           type === "image" ? (
             <img src={dataUrl} className={classes.image} />
           ) : (
-            <video src={dataUrl} className={classes.image} />
+            <video preload="metadata" controls={false} src={dataUrl} className={classes.image} />
           )
         ) : (
           <Skeleton variant="rect" width="100%" height="300px" />
